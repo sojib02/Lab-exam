9d84428c5472a03ec92db9935e3adb33a65ca000
Sojib <shojibmia626@yahoo.com>
2022-12-06 20:14:40 +0600

    lab exam

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 5,
+    arrows: false,
+    focusOnSelect: true,
+    autoplay: true,
+    autoplaySpeed: 2000, 
+  });
